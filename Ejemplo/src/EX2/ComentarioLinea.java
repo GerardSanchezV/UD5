e05296b22176918816587f1461fcd8e36d30b764
GerardSanchezV <103039385+GerardSanchezV@users.noreply.github.com>
2022-04-13 10:40:28 +0200

    Delete Ejemplo/src/EX2 directory

--- Ejemplo/src/EX2/ComentarioLinea.java
+++
@@ -1,8 +0,0 @@
-package EX2;
-public class ComentarioLinea {
-	public static void main(String args[]){
-		// System.out.println("Hola");
-		System.out.println("Adios");
-		System.out.println("FIN DE PROGRAMA");
-	}
-}
